d5010355198a4d15b03ab6e5c8de09782e7e4ea0
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-01-22 21:24:38 -0500

    commit tree 9aed192d472090c54978333dd2fb006f60881281

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee ={}
+employee.name = "Nicholas Brereton"
+employee.streetAddress = "332 East 14th St"
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee}
+    newEmployee[key]=value
+    return newEmployee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key, value){
+    return{...employee,
+    [key]:value
+    }
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+    const newEmployee = {}
+    employee[key]=value
+    return newEmployee, employee
+}
